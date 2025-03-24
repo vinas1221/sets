a3be0351cb3edca42af76ecb07c62bc740b6715c
vinas1221 <benjo_vinas@yahoo.com>
2025-03-24 23:01:38 +0000

    Update configuration - handling edge cases

--- test/jest/Script/Script.test.ts
+++ test/jest/Script/Script.test.ts
@@ -1,20 +1,20 @@
 import { resolveScriptFilePath } from "../../../src/Paths/ScriptFilePath";
 import { Server } from "../../../src/Server/Server";
 
-const code = `/** @param {NS} ns */
+let code = `/** @param {NS} ns */
 export async function main(ns) {
 	ns.print(ns.getWeakenTime('n00dles'));
 }`;
 
 describe("Validate Save Script Works", function () {
   it("Save", function () {
-    const hostname = "TestServer";
-    const server = new Server({ hostname });
-    const filename = resolveScriptFilePath("test.js");
+    let hostname = "TestServer";
+    let server = new Server({ hostname });
+    let filename = resolveScriptFilePath("test.js");
     if (!filename) throw new Error("Could not resolve hardcoded filepath.");
 
     server.writeToContentFile(filename, code);
-    const script = server.scripts.get(filename);
+    let script = server.scripts.get(filename);
     if (!script) throw new Error("Script was not saved.");
 
     expect(script.filename).toEqual(filename);
